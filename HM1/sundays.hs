2bee611b530e3b9824e604a13a433b9cc77009f5
ismail <i.namdar@yahoo.com>
2018-02-25 15:02:52 +0300

    dayOfWeek implemented

--- HM1/sundays.hs
+++ HM1/sundays.hs
@@ -1,1 +1,26 @@
-main = print "Hello Haskell"
+import Prelude
+
+monthCalculate :: Integer -> Integer
+monthCalculate m
+    | m <= 2 = m + 12
+    | otherwise = m
+
+dayOfWeek :: Integer -> Integer -> Integer -> Integer
+dayOfWeek y m d = (d + t1 + k + t2 + t3 + 5 * j) `mod` 7
+    where
+        m = monthCalculate m
+
+        j :: Integer
+        j = y `div` 100
+
+        k :: Integer
+        k = y `mod` 100
+
+        t1 :: Integer
+        t1 = 13 * (m + 1) `div` 5
+
+        t2 :: Integer
+        t2 = k `div` 4
+
+        t3 :: Integer
+        t3 = j `div` 4
